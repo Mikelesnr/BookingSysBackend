344708f9db16aae9ed469e1629d2130d9bf7a22b
Mikelesnr <mwanza.n.m@gmail.com>
2023-04-03 09:29:32 +0200

    view one driver, update driver, delete driver fuctionality

--- driver/views.py
+++ driver/views.py
@@ -7,9 +7,9 @@ from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
-def driver_list(request):
+def driver_list(request, format=None):
     '''
-    .get all the drivers
+    .get all the drivers and adds driver
     .serialize them
     .return json
     '''
@@ -18,7 +18,7 @@ def driver_list(request):
     if request.method == 'GET':
         drivers = Driver.objects.all()
         serializer = DriverSerializer(drivers, many=True)
-        return JsonResponse({'drivers': serializer.data})
+        return Response(serializer.data)
 
     # add drivers
     if request.method == 'POST':
@@ -26,3 +26,36 @@ def driver_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def driver_detail(request, id, format=None):
+    '''
+    .gets one driver, update driver, delete driver
+    .serializes record
+    .return json
+    '''
+
+    # retrieve driver and assign to driver
+    try:
+        driver = Driver.objects.get(pk=id)
+    except Driver.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get driver
+    if request.method == 'GET':
+        serializer = DriverSerializer(driver)
+        return Response(serializer.data)
+
+    # edit driver
+    elif request.method == 'PUT':
+        serializer = DriverSerializer(driver, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # delete driver
+    elif request.method == 'DELETE':
+        driver.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
