bfbfd2e5767af1f384ae6bdaf90270a6cf36faa4
Mikelesnr <mwanza.n.m@gmail.com>
2023-04-03 08:15:29 +0200

    Add driver and bus route created

--- driver/views.py
+++ driver/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
 from .models import Driver
 from .serializers import DriverSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET', 'POST'])
 def driver_list(request):
     '''
     .get all the drivers
@@ -10,6 +14,15 @@ def driver_list(request):
     .return json
     '''
 
-    drivers = Driver.objects.all()
-    serializer = DriverSerializer(drivers, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    # get drivers
+    if request.method == 'GET':
+        drivers = Driver.objects.all()
+        serializer = DriverSerializer(drivers, many=True)
+        return JsonResponse({'drivers': serializer.data})
+
+    # add drivers
+    if request.method == 'POST':
+        serializer = DriverSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
